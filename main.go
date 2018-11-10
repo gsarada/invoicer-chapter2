91ffb2412f62f59af2d127356699028936c45682
root <root@debian>
2018-11-10 07:52:09 -0500

    correction

--- main.go
+++ main.go
@@ -203,7 +203,8 @@ func (iv *invoicer) deleteInvoice(w http.ResponseWriter, r *http.Request) {
 
 func (iv *invoicer) getIndex(w http.ResponseWriter, r *http.Request) {
 	log.Println("serving index page")
-        w.Header().Add("Content-Security-Policy", "default-src 'self'; child-src 'self';")
+        w.Header().Add("Content-Security-Policy", "default-src 'self'; child-src 'self'; script-src 'self';")
+        w.Header().Add("x-xss-protection","0")
         w.Header().Add("X-Frame-Options", "SAMEORIGIN")
 	w.Write([]byte(`
 <!DOCTYPE html>
